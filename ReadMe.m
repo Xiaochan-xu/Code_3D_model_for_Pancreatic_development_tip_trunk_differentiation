3c609bfcc4e8013cf98249bc32e1baf88690aa8c
Xiaochan Xu <58542579+Xiaochan-xu@users.noreply.github.com>
2022-09-09 10:33:17 +0200

    Add files via upload

---
+++ ReadMe.m
@@ -0,0 +1,27 @@
+%% Short description of codes
+
+%% Main_0_Construct_3D_structure.m
+% Construct a 3D organ with 400 cells in total.
+
+%% Main_1_Fig2_SuppFig2.m
+% 1) Simulate a two-cell system with different strengths of Cis-interaction
+% and Trans-interaction.
+% 2) Derived a two-cell model to investigat delay in Dll1 transcription.
+
+%% Main_2_Multi_cell_organ_simulations.m
+% Simulate different conditions with multi-cell organ model 
+
+%% Main_3_Fig3_4_5_SuppFigs_Dynamics_on_3D.m
+% 1) Plot of wildtype, Dll1 deficient, Jag1 deficient, DAPT, MLN4927
+% 2) Simulate time delay in Dll1 transcription with the multi-cell model
+% 3) Compare dynamics with removing cis- or trans- interaction of Dll1 and Jag1 
+
+%% Main_4_SuppFig6_slow_fast_Ptf1a.m
+% Compare dynamics with different time scales of Ptf1a
+
+%% Main_5_SuppFig7_8_sensitivity_analysis.m
+% 1) Analyze cell fate segregation with two-cell model by changing each parameter from 1% to 200% 
+% 2) Analyze period of oscillation with two-cell model by changing each parameter from 1% to 200%
+
+%% Main_6_Supp_Movies.m
+% Make movies of wildtype, Dll1 deficient, Jag1 deficient, DAPT, MLN4927
